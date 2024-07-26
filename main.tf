3f94a46bb0d92857c727ce6a559f60b5b567e610
prashanth4971 <prashanth4971@gmail.com>
2024-07-26 16:58:35 +0530

    C2

--- main.tf
+++ main.tf
@@ -1,1 +1,68 @@
+provider "aws" {
+
+
+  region = "us-east-1"
+
+
+
+}
+
+
+
+variable "create_vpc" {
+
+  type = bool
+
+}
+
+
+
+
+
+resource "aws_vpc" "dep1" {
+
+
+
+    count = var.create_vpc ? 1:0
+
+
+
+    cidr_block = "192.168.0.0/16"
+
+    tags = {
+
+      "Name" = "VPC1"
+
+      
+
+    }
+
+  
+
+}
+
+
+
+resource "aws_vpc" "dep2" {
+
+
+
+    count = var.create_vpc ? 0:1
+
+
+
+    cidr_block = "192.168.1.0/24"
+
+    tags = {
+
+      "Name" = "VPC2"
+
+      
+
+    }
+
+  
+
+}
+
 
